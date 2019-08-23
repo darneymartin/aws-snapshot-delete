e67a7236941dd1d8b5996ae5a48dcf5c770bc245
Darnell Martin <darnell.martin@sagenet.com>
2019-08-23 13:29:53 -0400

    Added ability to match description

--- delete_snapshots.py
+++ delete_snapshots.py
@@ -3,6 +3,7 @@ import sys
 import boto3
 import argparse
 import json
+import re
 from datetime import datetime, timedelta, timezone
 
 ARG_HELP ="""
@@ -17,26 +18,21 @@ ARG_HELP ="""
     --------------------------------------------------------------------------------
     """
 
-class JsonAction(argparse.Action):
-    """
-    Custom action that gets json
-    """
-
-    def __call__(self, parser, namespace, value, option_string=None):
-        value = {"Filters": json.loads(value)}
-        setattr(namespace, self.dest, value)
-
 def main(args):
     session = boto3.Session(profile_name=args.profile)
     ec2 = session.client('ec2')
-    snapshots = ec2.describe_snapshots(**args.filter)
+    snapshots = ec2.describe_snapshots(OwnerIds=['self'])
+    if args.description is not None:
+        pattern = re.compile(args.description)
+    else:
+        pattern = re.compile(r".*")
     print("Deleting any snapshots older than {0} days".format(args.age))
     delete_time = datetime.now(timezone.utc) - timedelta(days=args.age)
     size_counter = 0
     delete_count = 0
     for snapshot in snapshots["Snapshots"]:
         start_time = snapshot['StartTime']
-        if start_time < delete_time:
+        if((start_time < delete_time) and (re.search(pattern,snapshot['Description']) is not None)):
             size_counter = size_counter + snapshot['VolumeSize']
             delete_count = delete_count + 1
             if args.delete is True:
@@ -54,11 +50,12 @@ def main(args):
 if __name__ == '__main__':
     try:
         args = argparse.ArgumentParser(description=ARG_HELP, formatter_class=argparse.RawTextHelpFormatter, usage=argparse.SUPPRESS)
-        args.add_argument('--filter','-f', action=JsonAction, default={}, help="JSON formatted string to filter Snapshots")
+        #args.add_argument('--filter','-f', action=JsonAction, default={}, help="JSON formatted string to filter Snapshots")
         args.add_argument('--profile','-p', dest='profile', type=str, default="default", help="Profile to use (Default: default)")
         args.add_argument('--age','-a', dest='age', type=int, default=0, help='The max age in days you want to keep')
-        args.add_argument('--delete','-d', dest='delete', action='store_true', help='Specify to delete Snapshots')
-        args.add_argument('--verbose','-v', dest="verbose", action='store_true', help="Show verbose output of program")
+        args.add_argument('--delete','-d', dest='delete', action='store_true', help="Specify to delete Snapshots")
+        args.add_argument('--verbose','-v', dest='verbose', action='store_true', help="Show verbose output of program")
+        args.add_argument('--description', dest='description', type=str, default=None, help="Description of snapshot(s) can be a regex")
         args = args.parse_args()
         # Launch Main
         main(args)
